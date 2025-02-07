090f9c3d417abce1aa83c7d31a0fc58e2130d530
Aaron <aaron.karr11@gmail.com>
2025-02-06 21:50:27 -0800

    need to clean up code and add more polish

---
+++ IcoConverter/IConverterLogic.cs
@@ -0,0 +1,8 @@
+﻿namespace IcoConverter
+{
+    public interface IConverterLogic
+    {
+        byte[] ConvertImageToIcon(string inputImagePath, int size);
+        void ConvertImageToIcon(string inputImagePath, string outputIconPath);
+    }
+}
--- IcoConverter/MainWindow.xaml.cs
+++ IcoConverter/MainWindow.xaml.cs
@@ -1,29 +1,54 @@
-﻿using System.Text;
+﻿using System.Diagnostics.Metrics;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace IcoConverter
 {
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string _inputURL;
+        private string _outputURL;
+        private IConverterLogic _converterLogic;
+
         public MainWindow()
         {
+            _converterLogic = new ConverterLogic();
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void btnConvert_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                _inputURL = inutPath.Text;
+                _outputURL = outputPath.Text;
+
+                string newFilePath = String.Empty;
+                //byte[] bytes = File.ReadAllBytes(_inputURL);
+                var newFile = _converterLogic.ConvertImageToIcon(_inputURL, 64);
+                newFilePath = $"{_outputURL}\\{DateTime.Now.ToString("yyyyMMddhhmmss")}.ico";
+                //_converterLogic.ConvertImageToIcon(_inputURL, newFilePath);
+                File.WriteAllBytes(newFilePath, newFile);
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
 
         }
+
+        //private void RenderLoadingBar(int total, int current)
+        //{
+        //    progressBar1.PerformStep();
+        //    if (total == current)
+        //    {
+        //        lblOutput.Text = "All Done! 😊";
+        //    }
+        //}
     }
 }
