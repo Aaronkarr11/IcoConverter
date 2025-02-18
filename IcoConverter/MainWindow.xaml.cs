2101c46b9ce037c6b78c28bf7b088994861316d4
Aaron <aaron.karr11@gmail.com>
2025-02-17 20:41:24 -0800

    daily

--- IcoConverter/MainWindow.xaml.cs
+++ IcoConverter/MainWindow.xaml.cs
@@ -1,6 +1,10 @@
 ﻿using System.Diagnostics.Metrics;
 using System.IO;
+using System.Net.NetworkInformation;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media.Animation;
 
 namespace IcoConverter
 {
@@ -24,6 +28,7 @@ namespace IcoConverter
         {
             try
             {
+                pbStatus.Value = 0;
                 _inputURL = inutPath.Text;
                 _outputURL = outputPath.Text;
 
@@ -31,8 +36,10 @@ namespace IcoConverter
                 //byte[] bytes = File.ReadAllBytes(_inputURL);
                 var newFile = _converterLogic.ConvertImageToIcon(_inputURL, 64);
                 newFilePath = $"{_outputURL}\\{DateTime.Now.ToString("yyyyMMddhhmmss")}.ico";
-                //_converterLogic.ConvertImageToIcon(_inputURL, newFilePath);
+
+                RenderLoadingBar(100);
                 File.WriteAllBytes(newFilePath, newFile);
+
             }
             catch (Exception ex)
             {
@@ -42,13 +49,27 @@ namespace IcoConverter
 
         }
 
-        //private void RenderLoadingBar(int total, int current)
-        //{
-        //    progressBar1.PerformStep();
-        //    if (total == current)
-        //    {
-        //        lblOutput.Text = "All Done! 😊";
-        //    }
-        //}
+        //pbStatus
+
+        private void RenderLoadingBar(int seed)
+        {
+            for (int i = 0; i < seed; i++)
+            {
+                pbStatus.Value++;
+            }
+        }
+
+        private void inutPath_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            inutPath.Text = "";
+        }
+
+        private void inutPath_GotFocus(object sender, RoutedEventArgs e)
+        {
+            inutPath.Text = "";
+        }
     }
+
+
+
 }
